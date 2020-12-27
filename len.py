77dccd7bf25a3162e35b727f6c01913e1eb6df15
closer <1753704997@qqcom>
2020-12-27 17:36:23 +0800

    第一次向github提交代码

--- len.py
+++
@@ -1,2 +0,0 @@
-a = 'qqwekcjdisncjsdsdbjcnvsjnsidjsfi idsjfisdi sic i' # 单引号和双引号都行，成对即可
-print(len(a))
--- practice.py
+++
@@ -1,4 +0,0 @@
-a = float(input("请输入："))
-b = float(input("请输入："))
-print("input获取的内容相加等于：",a+b)
-print("it's perfect！")
